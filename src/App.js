3bd59a1a78a23d349ba17c55a0e924cf7eb1be72
klequis <klequis@gmail.com>
2019-05-09 16:36:45 -0700

    print returned data to screen

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const codeStyle = {
+  color: 'white',
+  textAlign: 'left'
+}
 class App extends React.Component {
-  async componentDidMount() {
-    const r1 = await fetch('https://api.klequis-todo.tk')
-    console.log('r1', await r1.json())
+
+  state = {
+    data: undefined
   }
+  async componentDidMount() {
+    const r1 = await fetch("https://api.klequis-todo.tk");
+    const data = await r1.json()
+    this.setState({ data: data })
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            <pre style={codeStyle}>{JSON.stringify(this.state.data, null, 4)}</pre>
+          </div>
         </header>
       </div>
     );
